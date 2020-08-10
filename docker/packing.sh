cd199d1b9cc8ba4f65ccbe9e2b76cb78ba5c9310
周涛 <zhoutao825638@vip.qq.com>
2020-08-10 16:35:57 +0800

    :wrench: update build script

--- docker/packing.sh
+++ docker/packing.sh
@@ -1,6 +1,12 @@
 echo "Start build....."
 ./gradlew  build -x test
+
+if [[ $? -ne 0 ]]; then
+  echo "编译失败....."
+fi
+
+
 echo "Build Complete!"
 cp ./web/build/libs/web-1.0-SNAPSHOT.jar ./docker/blog.jar
-docker build ./docker -t registry.cn-shanghai.aliyuncs.com/default-tao/blog:20200717-2
-docker push registry.cn-shanghai.aliyuncs.com/default-tao/blog:20200717-2
+docker build ./docker -t registry.cn-shanghai.aliyuncs.com/default-tao/blog:2020810
+docker push registry.cn-shanghai.aliyuncs.com/default-tao/blog:2020810

f00afcec456834d6a911a9e88c1fc30e89d1d2b5
周涛 <zhoutao825638@vip.qq.com>
2021-03-06 21:59:13 +0800

    feat: 新增文件变动记录

--- src/main/java/com/mafour/api/controller/book/BookController.java
+++ src/main/java/com/mafour/api/controller/book/BookController.java
@@ -8,11 +8,10 @@ import com.mafour.api.service.book.bean.BookUpdateRecord;
 import com.mafour.api.service.redis.RedisService;
 import com.mafour.api.service.seo.SeoService;
 import lombok.Data;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +24,9 @@ public class BookController {
 
   @Autowired private BookRecordService bookRecordService;
 
-  private final BookService bookService;
+  @Autowired private BookService bookService;
 
-  private final RedisService redisService;
+  @Autowired private RedisService redisService;
 
   @Autowired private SeoService seoService;
 
